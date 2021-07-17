c7984e6b9b62af63df6a1f2bbeee0eeccaf7622a
slnamtarn <6210110566@psu.ac.th>
2021-07-17 17:34:19 +0700

    Flex Box

--- components/Weather.js
+++ components/Weather.js
@@ -22,6 +22,7 @@ export default function Weather(props){
 
 const styles = StyleSheet.create({
     backdrop: {
+        flexDirection: 'column',
         alignItems: 'center',
         width: '100%',
         height: '100%'
